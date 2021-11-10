8a977a2661f99ced5c2c59d55178921e22914af6
VasylShcherbyna <75271842+VasylShcherbyna@users.noreply.github.com>
2021-11-10 01:10:01 +0100

    Searchbar, Toast

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Searchbar from './Componnent/Searchbar/Searchbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
- <h>start</h>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+    image: '',
+  };
+  handleFormSubmit = image => {
+    this.setState({ image });
+  }
+  componentDidMount() {
+    fetch(
+      'https://pixabay.com/api/?key=22516391-185885990a61958acb3a57b33&q=yellow+flowers&image_type=photo&pretty=true',
+    )
+      .then(res => res.json())
+      .then(image => this.setState({ image }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ToastContainer position="top-center" autoClose={3000} />
+      </div>
+    );
+  }
 }
 
-export default App;
+
---
+++ src/Componnent/Searchbar/Searchbar.js
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import s from '../Styles.Module.css';
+
+export default class Searchbar extends Component {
+  state = {
+    imageName: '',
+  };
+  handleNameChange = e => {
+    this.setState({ imageName: e.currentTarget.value.toLowerCase() });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.imageName.trim() === '') {
+      // alert('Введите имя картинки.');
+      toast.error('Введите имя картинки.');
+      return;
+    }
+
+    this.props.onSubmit(this.state.imageName);
+    this.setState({ imageName: '' });
+  };
+
+  render() {
+    return (
+      <header className={s.Searchbar}>
+        <form className={s.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={s.SearchFormButton}>
+            <span className={s.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={s.SearchFormInput}
+            type="text"
+            name="imageName"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+            value={this.state.imageName}
+            onChange={this.handleNameChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
